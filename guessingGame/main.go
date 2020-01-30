0c22d0d571549e83960be380369a4492c3a57101
Di <55582048+didjeru@users.noreply.github.com>
2020-01-30 22:35:49 +0700

    Create guessing game

---
+++ guessingGame/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+)
+
+func getRandomNumber(numberMin, numberMax int) int {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("You are chiter!")
+			os.Exit(3)
+		}
+	}()
+	return numberMin + rand.Intn((numberMax)-(numberMin))
+}
+
+func inputData(msg string) string {
+	var data string
+	fmt.Print(msg)
+	fmt.Scanln(&data)
+	if data == "exit" {
+		os.Exit(3)
+	}
+	return data
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	fmt.Println("Guessing game!\nThink of a number in the range of 0 to about 100")
+	var numberMin, numberMax = 0, 100
+	compNumber := getRandomNumber(numberMin, numberMax)
+	fmt.Println("Your number", compNumber, "?")
+	countTry := 0
+	for {
+		num := inputData("Enter symbol < or = or > :")
+		if num == "<" || num == "=" || num == ">" {
+			countTry++
+		}
+		switch num {
+		case "=":
+			fmt.Println("Win witn", countTry, "try. And number -", compNumber)
+			os.Exit(3)
+		case ">":
+			numberMin = compNumber + 1
+			compNumber = getRandomNumber(numberMin, numberMax)
+			fmt.Println("Your number", compNumber, "?")
+		case "<":
+			numberMax = compNumber
+			compNumber = getRandomNumber(numberMin, numberMax)
+			fmt.Println("Your number", compNumber, "?")
+		default:
+			fmt.Println("Wrong symbol")
+		}
+	}
+}
